ad06d321e614a581f85ef924170f0310390b7a82
developervinay1 <developervsandhu@gmail.com>
2023-04-06 11:35:20 +0530

    React native project LinkedIn Clone User Interface only

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
+import Navigator from "./screens/Navigator";
+import { NavigationContainer } from "@react-navigation/native";
+import Header from "./screens/Header";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <Header />
+      </SafeAreaView>
+      <Navigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
